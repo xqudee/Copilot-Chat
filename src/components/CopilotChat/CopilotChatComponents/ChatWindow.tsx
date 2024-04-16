f5c73e1cf4b5046e8d618f4433af555f106389da
xqudee <annamorozenko2004@gmail.com>
2024-04-16 18:32:55 +0300

    Update ChatWindow.tsx

--- src/components/CopilotChat/CopilotChatComponents/ChatWindow.tsx
+++ src/components/CopilotChat/CopilotChatComponents/ChatWindow.tsx
@@ -4,7 +4,7 @@ import MessageInput from './MessageInput'
 
 const ChatWindow = () => {
   return (
-    <div className='p-[16px] border rounded-lg w-[40%] h-[80%] flex flex-col  gap-[8px]'>
+    <div className='p-[16px] border rounded-lg w-[90%] h-[90%] md:w-[50%] md:h-[80%] flex flex-col  gap-[8px]'>
       <div className='flex py-[8px] justify-between'>
         <h1 className='font-[600] text-[20px] leading-[24px]'>Financial Copilot Chat</h1>
 
